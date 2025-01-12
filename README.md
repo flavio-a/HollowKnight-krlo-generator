df71e11417d5e91c61c794e095473fc08f49b46f
Flavio <flavio.a.618@gmail.com>
2025-01-12 09:44:13 +0100

    Added settings section

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # HollowKnight-krlo-generator
+
+To run locally, install npm dev-dependencies and then:
+```
+npm run start
+```
+
+This starts the development server, listening on port 8080.
+
+## TODO
+- deploy on gh-pages
+- write credits in the page
+- add setting to group together levers close by
--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,35 @@ import { outputOrder } from "./make_output";
 import logic from "../../resources/krlo-logic.txt";
 import names from "../../resources/leverLongNames.json";
 
+let groupingFactor = 5;
+let difficulty = 1;
+
+const inputGroupingFactorValidation = document.getElementById(
+  "inputGroupingFactorValidation"
+);
+function inputGroupingFactorOnchange(e) {
+  const val = e.target.value;
+  if (val < 0 || val > 10) {
+    e.target.classList.add("is-invalid");
+    inputGroupingFactorValidation.classList.remove("invisible");
+  } else {
+    groupingFactor = val;
+    e.target.classList.remove("is-invalid");
+    inputGroupingFactorValidation.classList.add("invisible");
+  }
+}
+document.getElementById("inputGroupingFactor").onchange =
+  inputGroupingFactorOnchange;
+
+function inputDifficultyOnchange(e) {
+  difficulty = e.target.value;
+}
+document.getElementById("inputDifficulty").onchange = inputDifficultyOnchange;
+
 document.getElementById("generateButton").onclick = () => {
-  const difficulty = document.getElementById("inputDifficulty");
-  console.log(difficulty);
+  console.log(`Difficulty: ${difficulty}\nGrouping factor: ${groupingFactor}`);
   const order = getRandomOrder(logic, Object.keys(names), 1);
   outputOrder(order, names);
 };
 
-// Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
+document.getElementById("settingsForm").reset();
